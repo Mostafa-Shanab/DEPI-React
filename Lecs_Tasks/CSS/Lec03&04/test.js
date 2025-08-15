7d0f83f766b270eaf3d66c58bf1e6ba8d89aa45d
Mostafa Shanab <2021170523@cis.asu.edu.eg>
2025-08-15 22:40:10 +0300

    initialized Template1 page1 figma

---
+++ Lecs_Tasks/CSS/Lec03&04/test.js
@@ -0,0 +1,13 @@
+var x = 20;
+switch (x) {
+  case 10:
+    console.log(" This is case 10");
+  case 20:
+    console.log(" This is case 20");
+  case 30:
+    console.log(" This is case 30");
+  case 40:
+    console.log(" This is case 40");
+  default:
+    console.log("This is the default case");
+}
