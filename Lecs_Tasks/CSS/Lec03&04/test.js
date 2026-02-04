b3f28f9aa9a15e47b52838a5a8219ed290c5dc08
Mostafa Shanab <2021170523@cis.asu.edu.eg>
2026-02-04 22:07:38 +0200

    finished DEPI

--- Lecs_Tasks/CSS/Lec03&04/test.js
+++ Lecs_Tasks/CSS/Lec03&04/test.js
@@ -1,13 +1,4 @@
-var x = 20;
-switch (x) {
-  case 10:
-    console.log(" This is case 10");
-  case 20:
-    console.log(" This is case 20");
-  case 30:
-    console.log(" This is case 30");
-  case 40:
-    console.log(" This is case 40");
-  default:
-    console.log("This is the default case");
-}
+var x = "asd  ddd";
+// Array Methods
+console.log(x.split(" "));
+// String Methods
